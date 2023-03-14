b0788710903f691fff3b603032939e23cff8cb12
GAudrey <gilmantaudrey.ag@gmail.com>
2023-03-14 15:23:20 +0100

    exo fin

---
+++ app/controllers/bands/band/songs.js
@@ -0,0 +1,43 @@
+/* eslint-disable ember/no-computed-properties-in-native-classes */
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action, computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class BandsBandSongsController extends Controller {
+  @service store;
+
+  @tracked isAddingSong = false;
+  @tracked selectSong = {
+    title: '',
+  };
+
+  @computed('selectSong.title.{lenght,length}')
+  get isAddingButtonDisabled() {
+    return this.selectSong.title.length < 3;
+  }
+
+  @action
+  addingSong() {
+    this.isAddingSong = true;
+  }
+  @action
+  async addSong(e) {
+    e.preventDefault();
+    this.isAddingSong = false;
+    let record = this.store.createRecord('song', {
+      ...this.selectSong,
+      rating: 0,
+      band: this.model,
+    });
+    await record.save();
+
+    this.selectSong = {
+      title: '',
+    };
+  }
+  @action
+  cancelSong() {
+    this.addingSong = false;
+  }
+}
