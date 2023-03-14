455896388040821bc1e38fb7dcb066b333147be0
GAudrey <gilmantaudrey.ag@gmail.com>
2023-03-14 09:40:46 +0100

    finish model

--- app/models/song.js
+++ app/models/song.js
@@ -3,5 +3,6 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 export default class SongModel extends Model {
   @attr('string') title;
   @attr('number') rating;
+  // relation vers une autre entité
   @belongsTo('band') band;
 }
