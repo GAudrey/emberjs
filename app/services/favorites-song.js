7f8ce5e065298e71fe47bf7786009a45691e1c58
GAudrey <gilmantaudrey.ag@gmail.com>
2023-03-17 09:50:34 +0100

    services

---
+++ app/services/favorites-song.js
@@ -0,0 +1,10 @@
+import Service from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+
+export default class FavoritesSongService extends Service {
+  @tracked items = [];
+
+  add(item) {
+    this.items.pushObject(item);
+  }
+}
